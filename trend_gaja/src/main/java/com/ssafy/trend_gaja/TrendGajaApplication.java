67d87e21ae2253ee7b08ec61eefab4f17b7bf429
a50070863 <a50070863@gmail.com>
2023-11-01 00:09:03 +0900

    setting: 프로젝트 초기 환경 설정 (#3)

---
+++ trend_gaja/src/main/java/com/ssafy/trend_gaja/TrendGajaApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.trend_gaja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendGajaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendGajaApplication.class, args);
+	}
+
+}
---
+++ trend_gaja/src/test/java/com/ssafy/trend_gaja/TrendGajaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.trend_gaja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrendGajaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
