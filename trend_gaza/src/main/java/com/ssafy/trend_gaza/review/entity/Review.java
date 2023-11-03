895add345846eeb9050808d240225f38a3f185b2
a50070863 <a50070863@gmail.com>
2023-11-03 23:30:16 +0900

    feat: 리뷰 Entity 생성 (#18)

---
+++ trend_gaza/src/main/java/com/ssafy/trend_gaza/review/entity/Review.java
@@ -0,0 +1,52 @@
+package com.ssafy.trend_gaza.review.entity;
+
+import java.time.LocalDateTime;
+
+public class Review {
+	
+	private int questionId;
+	private String content;
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
+	private String userId;
+	
+	
+	public int getQuestionId() {
+		return questionId;
+	}
+	public void setQuestionId(int questionId) {
+		this.questionId = questionId;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Review [questionId=" + questionId + ", content=" + content + ", createdAt=" + createdAt + ", updatedAt="
+				+ updatedAt + ", userId=" + userId + "]";
+	}
+	
+}
