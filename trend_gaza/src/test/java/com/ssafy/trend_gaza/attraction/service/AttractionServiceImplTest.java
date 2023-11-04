a3c3896329ed10fac87de2ecb5d460c8d84c883e
a50070863 <a50070863@gmail.com>
2023-11-04 18:25:42 +0900

    test: 서비스 테스트 코드 추가 (#23)

--- trend_gaza/src/test/java/com/ssafy/trend_gaza/attraction/service/AttractionServiceImplTest.java
+++ trend_gaza/src/test/java/com/ssafy/trend_gaza/attraction/service/AttractionServiceImplTest.java
@@ -6,13 +6,16 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.ssafy.trend_gaza.attraction.dto.AttractionAdminRequest;
 import com.ssafy.trend_gaza.attraction.entity.AttractionInfo;
 
 import lombok.extern.slf4j.Slf4j;
@@ -45,6 +48,17 @@ class AttractionServiceImplTest {
 		assertEquals(15, attractionInfo.size());
 		
 		//log.debug("관광지 조회 테스트 종료");
+	}
+	
+	@Test
+	@DisplayName("[관리자 권한] 관광지 등록 테스트")
+	@Transactional
+	void testRegisterAdminAttraction() {
+				
+		AttractionAdminRequest attractionAdminRequest = new AttractionAdminRequest(2856807, 12, "관광지명",
+				"주소", "연락처", "이미지 경로", 1.0, 1.0, 32, 18);
+		
+		attractionService.registerAdminAttraction(attractionAdminRequest);
 	}
 
 }
