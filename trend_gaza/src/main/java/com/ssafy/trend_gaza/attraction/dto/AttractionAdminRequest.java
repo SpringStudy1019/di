c53e690fba46e7280bd04e7a04b831e44dd9b86f
a50070863 <a50070863@gmail.com>
2023-11-04 22:30:23 +0900

    refactor: DTO에 빌더 패턴 적용 (#7)

--- trend_gaza/src/main/java/com/ssafy/trend_gaza/attraction/dto/AttractionAdminRequest.java
+++ trend_gaza/src/main/java/com/ssafy/trend_gaza/attraction/dto/AttractionAdminRequest.java
@@ -16,89 +16,9 @@ public class AttractionAdminRequest {
 	private Integer sidoCode;
 	private Integer gugunCode;
 
+	
 	public AttractionAdminRequest() {
 	}
-
-	public Integer getAttractionId() {
-		return attractionId;
-	}
-
-	public void setAttractionId(Integer attractionId) {
-		this.attractionId = attractionId;
-	}
-
-	public Integer getContentTypeId() {
-		return contentTypeId;
-	}
-
-	public void setContentTypeId(Integer contentTypeId) {
-		this.contentTypeId = contentTypeId;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getTel() {
-		return tel;
-	}
-
-	public void setTel(String tel) {
-		this.tel = tel;
-	}
-
-	public String getDefaultImg() {
-		return defaultImg;
-	}
-
-	public void setDefaultImg(String defaultImg) {
-		this.defaultImg = defaultImg;
-	}
-
-	public double getLatitude() {
-		return latitude;
-	}
-
-	public void setLatitude(double latitude) {
-		this.latitude = latitude;
-	}
-
-	public double getLongitude() {
-		return longitude;
-	}
-
-	public void setLongitude(double longitude) {
-		this.longitude = longitude;
-	}
-
-	public Integer getSidoCode() {
-		return sidoCode;
-	}
-
-	public void setSidoCode(Integer sidoCode) {
-		this.sidoCode = sidoCode;
-	}
-
-	public Integer getGugunCode() {
-		return gugunCode;
-	}
-
-	public void setGugunCode(Integer gugunCode) {
-		this.gugunCode = gugunCode;
-	}
-	
 
 	public AttractionAdminRequest(Integer attractionId, Integer contentTypeId, String title, String address, String tel,
 			String defaultImg, double latitude, double longitude, Integer gugunCode, Integer sidoCode) {
@@ -113,6 +33,47 @@ public class AttractionAdminRequest {
 		this.longitude = longitude;
 		this.gugunCode = gugunCode;
 		this.sidoCode = sidoCode;
+	}
+
+
+	public Integer getAttractionId() {
+		return attractionId;
+	}
+
+	public Integer getContentTypeId() {
+		return contentTypeId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public String getDefaultImg() {
+		return defaultImg;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public Integer getSidoCode() {
+		return sidoCode;
+	}
+
+	public Integer getGugunCode() {
+		return gugunCode;
 	}
 
 }
