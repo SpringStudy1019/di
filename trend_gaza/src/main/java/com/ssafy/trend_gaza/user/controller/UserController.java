ae19e1d2b7c173057d657bddc5b74d7f8c2f65b2
Juhyun Lee <juhyun62015@gmail.com>
2023-11-04 12:32:58 +0900

    feat: user view test code (#5)

--- trend_gaza/src/main/java/com/ssafy/trend_gaza/user/controller/UserController.java
+++ trend_gaza/src/main/java/com/ssafy/trend_gaza/user/controller/UserController.java
@@ -11,11 +11,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.trend_gaza.user.dto.ChangePwdRequest;
@@ -127,9 +127,8 @@ public class UserController {
 		
 	}
 	
-	@GetMapping(value = "/view")
-	public ResponseEntity<?> view(@RequestParam String userId) {
-		
+	@GetMapping(value = "/view/{userId}")
+	public ResponseEntity<?> view(@PathVariable String userId) {
 		try {
 			User user = userService.view(userId);
 			return new ResponseEntity<User>(user, HttpStatus.OK);
