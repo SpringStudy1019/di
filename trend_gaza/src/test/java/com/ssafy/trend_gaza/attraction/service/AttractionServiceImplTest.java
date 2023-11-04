d086eaad9508ad8f6e36749e5cb4c835c30b1545
a50070863 <a50070863@gmail.com>
2023-11-04 22:32:10 +0900

    test: 서비스 테스트 코드 수정 (#23)

--- trend_gaza/src/test/java/com/ssafy/trend_gaza/attraction/service/AttractionServiceImplTest.java
+++ trend_gaza/src/test/java/com/ssafy/trend_gaza/attraction/service/AttractionServiceImplTest.java
@@ -21,43 +21,42 @@ import com.ssafy.trend_gaza.attraction.entity.AttractionInfo;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@SpringBootTest(
-		properties = {
-				"spring.config.location=classpath:application.properties"
-		},
-		classes = {AttractionServiceImpl.class }
-)
+@SpringBootTest(properties = { "spring.config.location=classpath:application.properties" }, classes = {
+		AttractionServiceImpl.class })
 @AutoConfigureMockMvc
-@ComponentScan(basePackages = {"com.ssafy"})
+@ComponentScan(basePackages = { "com.ssafy" })
 class AttractionServiceImplTest {
-	
+
 	@Autowired
 	private AttractionService attractionService;
 
 	@Test
 	@DisplayName("관광지 조회 테스트")
 	void testSearchAttractions() {
-		//log.debug("관광지 조회 테스트 시작");
-		
+		// log.debug("관광지 조회 테스트 시작");
+
 		Map<String, String> map = new HashMap<String, String>();
 		map.put("sido", "32");
 		map.put("contentTypeId", "12");
 		map.put("keyword", "국립");
-		
+
 		List<AttractionInfo> attractionInfo = attractionService.searchAttractions(map);
 		assertEquals(15, attractionInfo.size());
-		
-		//log.debug("관광지 조회 테스트 종료");
+
+		// log.debug("관광지 조회 테스트 종료");
 	}
-	
+
 	@Test
 	@DisplayName("[관리자 권한] 관광지 등록 테스트")
 	@Transactional
 	void testRegisterAdminAttraction() {
-				
-		AttractionAdminRequest attractionAdminRequest = new AttractionAdminRequest(2856807, 12, "관광지명",
-				"주소", "연락처", "이미지 경로", 1.0, 1.0, 32, 18);
+
+		// given
+		AttractionAdminRequest attractionAdminRequest = AttractionAdminRequest.builder().attractionId(2856807)
+				.contentTypeId(12).title("관광지명").address("주소").tel("연락처").defaultImg("이미지 경로").latitude(1.0)
+				.longitude(1.0).build();
 		
+		// when
 		attractionService.registerAdminAttraction(attractionAdminRequest);
 	}
 
