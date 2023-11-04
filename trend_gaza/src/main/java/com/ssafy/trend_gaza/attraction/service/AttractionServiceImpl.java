b193aee0b7539f7c10d242e3b00b74f2101a084d
a50070863 <a50070863@gmail.com>
2023-11-04 18:24:48 +0900

    feat: 서비스단 추가 (#23)

--- trend_gaza/src/main/java/com/ssafy/trend_gaza/attraction/service/AttractionServiceImpl.java
+++ trend_gaza/src/main/java/com/ssafy/trend_gaza/attraction/service/AttractionServiceImpl.java
@@ -5,7 +5,10 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.ssafy.trend_gaza.attraction.dto.AttractionAdminRequest;
+import com.ssafy.trend_gaza.attraction.entity.AttractionDetail;
 import com.ssafy.trend_gaza.attraction.entity.AttractionInfo;
 import com.ssafy.trend_gaza.attraction.repository.AttractionMapper;
 
@@ -34,6 +37,17 @@ public class AttractionServiceImpl implements AttractionService {
 		
 		return attractionMapper.searchAttractions(map);
 		//return result.stream().map(AttractionResponse::of).toList();
+	}
+
+
+	@Override
+	//@Transactional
+	public void registerAdminAttraction(AttractionAdminRequest attractionAdminRequest) {
+		
+		attractionMapper.registerAdminAttraction(attractionAdminRequest);
+		//AttractionDetail attractionDetail = new AttractionDetail();
+		//attractionDetail.setContentId(contentId);
+		attractionMapper.registerAttractionDetail(attractionAdminRequest);
 	}
 
 }
