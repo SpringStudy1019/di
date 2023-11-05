83063cba34653f5bee5bb64d50c5ca830bc79640
Juhyun Lee <91438461+JulieOnIsland@users.noreply.github.com>
2023-11-05 20:05:44 +0900

    Update LoginRequest.java

--- trend_gaza/src/main/java/com/ssafy/trend_gaza/user/dto/LoginRequest.java
+++ trend_gaza/src/main/java/com/ssafy/trend_gaza/user/dto/LoginRequest.java
@@ -2,10 +2,11 @@ package com.ssafy.trend_gaza.user.dto;
 
 public class LoginRequest {
 
-	private final String userId;
-	private final String userPwd;
+	private  String userId;
+	private  String userPwd;
 	
-	
+	public LoginRequest() {}
+
 	public LoginRequest(String userId, String userPwd) {
 		super();
 		this.userId = userId;
