eecc05ddbae02843bcbff8e441b4009c5ed3ce12
a50070863 <a50070863@gmail.com>
2023-11-05 15:11:14 +0900

    feat: 이메일 발송 서비스 추가 (#30)

---
+++ trend_gaza/src/main/java/com/ssafy/trend_gaza/user/service/EmailService.java
@@ -0,0 +1,32 @@
+package com.ssafy.trend_gaza.user.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+	
+	@Value("${spring.mail.username}")
+    private String email;
+
+    private final JavaMailSender javaMailSender;
+    
+    
+    public EmailService(JavaMailSender javaMailSender) {
+		super();
+		//this.email = email;
+		this.javaMailSender = javaMailSender;
+	}
+
+
+	public void sendMail(String to, String sub, String text) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(to);
+        mailMessage.setFrom(email);
+        mailMessage.setSubject(sub);
+        mailMessage.setText(text);
+        javaMailSender.send(mailMessage);
+    }
+}
