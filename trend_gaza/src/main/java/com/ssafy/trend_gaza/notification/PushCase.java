31ad1537503de179ee626979a2216013e8f8925b
a50070863 <a50070863@gmail.com>
2023-11-06 19:37:55 +0900

    feat: 알림 도메인 추가 (#38)

---
+++ trend_gaza/src/main/java/com/ssafy/trend_gaza/notification/PushCase.java
@@ -0,0 +1,8 @@
+package com.ssafy.trend_gaza.notification;
+
+public enum PushCase {
+	COMMENT,
+	SYSTEM,
+	LIKE,
+	ADMIN
+}
---
+++ trend_gaza/src/main/java/com/ssafy/trend_gaza/notification/PushStatus.java
@@ -0,0 +1,6 @@
+package com.ssafy.trend_gaza.notification;
+
+public enum PushStatus {
+	COMPLETE,
+	IN_COMPLETE
+}
