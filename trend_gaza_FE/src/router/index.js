b38014d078fda565066a804e6065552287157be9
a50070863@gmail.com <a50070863@gmail.com>
2023-11-10 12:05:06 +0900

    init: [FE] Vue 초기 환경 설정

---
+++ trend_gaza_FE/src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import TheMainView from "@/views/TheMainView.vue"
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: TheMainView
+    },
+    
+  ]
+})
+
+export default router
