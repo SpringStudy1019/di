649cdbbf79f141aa656de9de6f267e4564bcd9a9
a50070863 <a50070863@gmail.com>
2023-11-11 11:54:54 +0900

    feat: S3 설정에서 빈 타입 변경 (#40)

--- trend_gaza/src/main/java/com/ssafy/trend_gaza/config/AwsS3Config.java
+++ trend_gaza/src/main/java/com/ssafy/trend_gaza/config/AwsS3Config.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
@@ -22,9 +23,9 @@ public class AwsS3Config {
     private String region;
 
     @Bean
-    public AmazonS3Client amazonS3Client() {
+    public AmazonS3 amazonS3Client() {
         BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
-        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+        return AmazonS3ClientBuilder.standard()
                 .withRegion(region)
                 .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
                 .build();
