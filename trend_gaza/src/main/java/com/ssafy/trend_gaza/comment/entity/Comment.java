3ce22c0de9a6fce12f6202158acae6917a804156
Juhyun Lee <juhyun62015@gmail.com>
2023-11-11 16:20:33 +0900

    feat: created comment entity (#56)

---
+++ trend_gaza/src/main/java/com/ssafy/trend_gaza/comment/entity/Comment.java
@@ -0,0 +1,51 @@
+package com.ssafy.trend_gaza.comment.entity;
+
+public class Comment {
+
+	private Integer commentIdx;
+	private String content;
+	private String registerDate;
+	private String modifyDate;
+	private String userId;
+	private Integer reviewIdx;
+	
+	
+	public Comment() {}
+
+	public Comment(Integer commentIdx, String content, String registerDate, String modifyDate, String userId,
+			Integer reviewIdx) {
+		super();
+		this.commentIdx = commentIdx;
+		this.content = content;
+		this.registerDate = registerDate;
+		this.modifyDate = modifyDate;
+		this.userId = userId;
+		this.reviewIdx = reviewIdx;
+	}
+
+	
+	public Integer getCommentIdx() {
+		return commentIdx;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public String getRegisterDate() {
+		return registerDate;
+	}
+
+	public String getModifyDate() {
+		return modifyDate;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public Integer getReviewIdx() {
+		return reviewIdx;
+	}
+	
+}
