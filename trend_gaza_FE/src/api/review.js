59d7ad8bec160c7d4c4d805ce06530771873bca0
a50070863 <a50070863@gmail.com>
2023-11-11 16:14:31 +0900

    feat: 라우터 추가 (#45)

--- trend_gaza_FE/src/api/review.js
+++ trend_gaza_FE/src/api/review.js
@@ -11,9 +11,29 @@ function list(param, success, fail) {
 function detailReview(reviewIdx, success, fail) {
     local.get(`${url}/view/${reviewIdx}`).then(success).catch(fail);
 }
+
+function registReview(review, success, fail) {
+    local.post(`{url}`).then(success).catch(fail);
+}
+
+function deleteReview(reviewIdx, success, fail) {
+    local.delete(`${url}/${reviewIdx}`).then(success).catch(fail);
+}
+
+function modifyReview(reviewIdx, success, fail) {
+    local.put(`${url}/${reviewIdx}`).then(success).catch(fail);
+}
+
+function getModifyReview(reviewIdx, success, fail) {
+    local.get(`${url}/modify/${reviewIdx}`).then(success).catch(fail);
+}
+
     
 export {
     list,
     detailReview,
-
+    deleteReview,
+    modifyReview,
+    getModifyReview,
+    registReview
 }
