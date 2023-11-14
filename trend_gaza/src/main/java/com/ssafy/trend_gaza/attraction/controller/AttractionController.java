41c67fbb67b79ea621074eaae40a483e8efd5d1a
a50070863@gmail.com <a50070863@gmail.com>
2023-11-14 16:00:47 +0900

    feat: 관광지 상세 조회 로직 추가 (#75)

--- trend_gaza/src/main/java/com/ssafy/trend_gaza/attraction/controller/AttractionController.java
+++ trend_gaza/src/main/java/com/ssafy/trend_gaza/attraction/controller/AttractionController.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -32,8 +33,8 @@ public class AttractionController {
 	
 	
 	@GetMapping("/{id}")
-	public ResponseEntity<AttractionDetailResponse> findAttraction() {
-		return ResponseEntity.ok(new AttractionDetailResponse());
+	public ResponseEntity<AttractionDetailResponse> findAttraction(@PathVariable int id) {
+		return ResponseEntity.ok(attractionService.findAttraction(id));
 	}
 	
 	@GetMapping
