4f7f560e362c7892200a89f417b97ed1997f3726
a50070863@gmail.com <a50070863@gmail.com>
2023-11-15 17:34:17 +0900

    style: 자동 완성 검색 연동 (#88)

--- trend_gaza_FE/src/views/TheMainView.vue
+++ trend_gaza_FE/src/views/TheMainView.vue
@@ -1,33 +1,17 @@
 <script setup>
 import { useRouter } from 'vue-router';
 import TheCategory from '../components/category/TheCategory.vue';
+import Search from '@/components/search/Search.vue';
 
 const router = useRouter();
 
 </script>
 
 <template>
-        <!-- 검색 -->
-        <div class='search'>
-        <div class="container text-center">
-            <div class="row">
-              <div class="col-3"></div>
-              <div class="col-6">
-                <form class="d-flex">
-                  <div class="input-group input-group-sm">
-                    <input
-                      type="text"
-                      class="form-control"
-                      placeholder="가고 싶은 여행지를 입력하세요"
-                    />
-                    <button class="btn btn-dark" type="button">검색</button>
-                  </div>
-                </form>
-              </div>
-              <div class="col-3"></div>
-            </div>
-          </div>
-        </div>
+
+        <!-- 검색바 -->
+        <Search />
+
         <!-- 카테고리 -->
         <TheCategory />
         
