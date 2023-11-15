20490f79dfeb3dbb342f83ffccdfaa4733f1238d
이주현 <juhyun62015@gmail.com>
2023-11-15 17:25:07 +0900

    feat: FE register (#83)

--- trend_gaza_FE/src/components/users/UserRegister.vue
+++ trend_gaza_FE/src/components/users/UserRegister.vue
@@ -1,8 +1,32 @@
 <script setup>
-
+import { ref, watch } from "vue";
 import { useRouter } from "vue-router";
+import { registUser } from "@/api/user";
 
 const router = useRouter();
+const user = ref({
+  "emailDomain": "",
+  "emailId": "",
+  "gender": "",
+  "mobile": "",
+  "password": "",
+  "userId": "",
+  "userName": ""
+})
+const pwdcheck = ref("");
+
+function registerUser() {
+ 
+  console.log("Form Data:", user.value);
+  registUser(user.value,
+    (response) => {
+      let msg = "회원가입이 완료되었습니다!"
+      alert(msg);
+      router.push({ name: "main" });
+    },
+    (error) => console.log(error)
+  )
+}
 
 </script>
 
@@ -18,32 +42,32 @@ const router = useRouter();
         <form>
           <div class="mb-3">
             <label for="username" class="form-label">이름 : </label>
-            <input type="text" class="form-control" placeholder="이름..." />
+            <input type="text" class="form-control" placeholder="이름..." name="userName" v-model="user.userName"/>
           </div>
           <div class="mb-3">
             <label for="userId" class="form-label">아이디 : </label>
-            <input type="text" class="form-control" placeholder="아이디..." />
+            <input type="text" class="form-control" placeholder="아이디..." name="userId" v-model="user.userId"/>
           </div>
           <div class="mb-3">
             <label for="userPwd" class="form-label">비밀번호 : </label>
-            <input type="text" class="form-control" placeholder="비밀번호..." />
+            <input type="text" class="form-control" placeholder="비밀번호..." name="userPwd" v-model="user.password"/>
           </div>
           <div class="mb-3">
             <label for="pwdcheck" class="form-label">비밀번호확인 : </label>
-            <input type="text" class="form-control" id="pwdcheck" placeholder="비밀번호확인..." />
+            <input type="text" class="form-control" id="pwdcheck" placeholder="비밀번호확인..." v-model="pwdcheck"/>
           </div>
           <div class="mb-3">
             <label for="userPwd" class="form-label">핸드폰 번호 : </label>
             <p>숫자만 입력하세요.</p>
-            <input type="text" class="form-control" placeholder="핸드폰번호..." />
+            <input type="text" class="form-control" placeholder="핸드폰번호..." name="mobile" v-model="user.mobile" />
           </div>
           <div class="mb-3">
-            <label for="emailId" class="form-label">이메일 : </label>
+            <label for="emailId" class="form-label" name="emailId" >이메일 : </label>
             <div class="input-group">
-              <input type="text" class="form-control" placeholder="이메일아이디" />
+              <input type="text" class="form-control" placeholder="이메일아이디" v-model="user.emailId" />
               <span class="input-group-text">@</span>
-              <select class="form-select" aria-label="이메일 도메인 선택">
-                <option selected>선택</option>
+              <select class="form-select" aria-label="이메일 도메인 선택" name="emailDomain" v-model="user.emailDomain">
+                <option selected value="">선택</option>
                 <option value="ssafy.com">싸피</option>
                 <option value="google.com">구글</option>
                 <option value="naver.com">네이버</option>
@@ -53,15 +77,15 @@ const router = useRouter();
           </div>
           <div class="mb-3">
             <label for="userPwd" class="form-label">성별 : </label>
-            <select class="form-select" aria-label="성별 선택">
-                <option selected>선택</option>
+            <select class="form-select" aria-label="성별 선택" name="gender" v-model="user.gender">
+                <option selected value="">선택</option>
                 <option value="MALE">남성</option>
                 <option value="FEMALE">여성</option>
                 <option value="UNDEFINED">밝히지 않음</option>
               </select>
           </div>
           <div class="col-auto text-center">
-            <button type="button" class="btn btn-outline-primary mb-3">회원가입</button>
+            <button type="button" class="btn btn-outline-primary mb-3" @click='registerUser'>회원가입</button>
             <button type="reset" class="btn btn-outline-success ms-1 mb-3">초기화</button>
           </div>
         </form>
