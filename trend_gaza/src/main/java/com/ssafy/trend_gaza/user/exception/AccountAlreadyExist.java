d92fb2d1c9e3db254df235658d88a4e2b3a3d170
a50070863@gmail.com <a50070863@gmail.com>
2023-11-16 17:37:08 +0900

    feat: 예외 처리 추가 (#91)

---
+++ trend_gaza/src/main/java/com/ssafy/trend_gaza/user/exception/AccountAlreadyExist.java
@@ -0,0 +1,13 @@
+package com.ssafy.trend_gaza.user.exception;
+
+import com.ssafy.trend_gaza.common.CustomException;
+import com.ssafy.trend_gaza.common.CustomExceptionStatus;
+
+public class AccountAlreadyExist extends CustomException {
+
+	public static final CustomException EXCEPTION = new AccountAlreadyExist();
+
+    private AccountAlreadyExist() {
+        super(CustomExceptionStatus.ACCOUNT_ALREADY_EXIST);
+    }
+}
---
+++ trend_gaza/src/main/java/com/ssafy/trend_gaza/user/exception/UserNotFoundException.java
@@ -0,0 +1,12 @@
+package com.ssafy.trend_gaza.user.exception;
+
+import com.ssafy.trend_gaza.common.CustomException;
+import com.ssafy.trend_gaza.common.CustomExceptionStatus;
+
+public class UserNotFoundException extends CustomException {
+	public static final CustomException EXCEPTION = new UserNotFoundException();
+
+    private UserNotFoundException() {
+        super(CustomExceptionStatus.ACCOUNT_NOT_FOUND);
+    }
+}
