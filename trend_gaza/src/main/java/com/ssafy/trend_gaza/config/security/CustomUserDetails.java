7cf25bb49ee525bb9198f448b098d7769c34deb2
likelasttime <46569105+likelasttime@users.noreply.github.com>
2023-11-16 19:42:49 +0900

    [Feature] Spring Security 적용 (#98)
    * build: 스프링 시큐리티 의존성 추가 (#91)
    
    * feat: 예외 처리 추가 (#91)
    
    * feat: security 설정 추가 (#91)
    
    * feat: JWT와 인증 Util 추가 (#91)
    
    * feat: 사용자와 관련한 enum 추가 (#91)
    
    * feat: 유저 도메인 및 DTO 수정 (#91)
    
    * feat: 유저 레포지토리단 추가 (#91)
    
    * feat: user 서비스단 추가 (#91)
    
    * feat: 이미지 업로드 시큐리티 권한 설정 (#91)
    
    * feat: 이미지 업로드 시 필요한 유저 아이디 가져오기 (#91)
    
    * refactor: ImageController 리팩토링 (#91)

---
+++ trend_gaza/src/main/java/com/ssafy/trend_gaza/config/security/CustomUserDetails.java
@@ -0,0 +1,60 @@
+package com.ssafy.trend_gaza.config.security;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.ssafy.trend_gaza.user.entity.User;
+
+public class CustomUserDetails implements UserDetails {
+
+	private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public CustomUserDetails(User account) {
+        this.user = account;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collect = new ArrayList<>();
+        collect.add(new SimpleGrantedAuthority(user.getAuthority().getRole()));
+        return collect;
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return this.user.getUserId();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
