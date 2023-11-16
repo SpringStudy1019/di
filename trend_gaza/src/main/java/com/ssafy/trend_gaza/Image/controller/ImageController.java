f17ac96308c9706be090316ce92c3a6f7970f48e
a50070863 <a50070863@gmail.com>
2023-11-17 00:21:26 +0900

    fix: CORS 에러 해결
    빈 등록할 때 Origin 주소를 잘못 적어서 CORS 에러 발생함
    Origin을 지정해줬기 때문에 다음에 컨트롤러에 있는 CrossOrigin
    어노테이션은 제거해야겠음

--- trend_gaza/src/main/java/com/ssafy/trend_gaza/Image/controller/ImageController.java
+++ trend_gaza/src/main/java/com/ssafy/trend_gaza/Image/controller/ImageController.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -19,6 +20,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 @RequestMapping("/images")
+@CrossOrigin("*")
 public class ImageController {
 
 	private final UploadService uploadService;
