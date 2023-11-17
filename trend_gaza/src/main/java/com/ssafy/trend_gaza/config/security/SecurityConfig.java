06375b642d51755677fe514baa97acaf87ffbeb4
likelasttime <46569105+likelasttime@users.noreply.github.com>
2023-11-17 17:18:20 +0900

    [Style] 자동 완성 검색 결과 클릭 시 상세 페이지로 이동 (#114)
    * feat: Origin 추가 (#113)
    
    Vue 서버가 포트가 5173 또는 5174를 사용하길래 둘다 등록함
    
    * sytle: 검색어 클릭 시 상세 페이지로 이동 (#113)

--- trend_gaza/src/main/java/com/ssafy/trend_gaza/config/security/SecurityConfig.java
+++ trend_gaza/src/main/java/com/ssafy/trend_gaza/config/security/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:5173", "http://localhost:5174"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setExposedHeaders(Arrays.asList("*"));
