0ab759ad3a4e2bb88f05b8da191ad4d95850a4d7
Juhyun Lee <juhyun62015@gmail.com>
2023-11-17 22:03:01 +0900

    feat: BE controller in progress 3 (#38)

--- trend_gaza/src/main/java/com/ssafy/trend_gaza/notification/controller/NotificationController.java
+++ trend_gaza/src/main/java/com/ssafy/trend_gaza/notification/controller/NotificationController.java
@@ -1,14 +1,21 @@
 package com.ssafy.trend_gaza.notification.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.trend_gaza.notification.dto.NotificationResponse;
+import com.ssafy.trend_gaza.notification.entity.Notification;
 import com.ssafy.trend_gaza.notification.service.NotificationService;
 
 @RestController
@@ -32,6 +39,30 @@ public class NotificationController {
 			return exceptionHandling(e);
 		}
 		
+	}
+	
+	@DeleteMapping
+	public ResponseEntity<?> delete(@RequestParam String notificationIdx) {
+		try {
+			notificationService.delete(notificationIdx);
+			return new ResponseEntity<>(HttpStatus.OK);
+		} catch (Exception e) {
+			return exceptionHandling(e);
+		}
+	}
+	
+	@GetMapping(value = "/{userIdTo}")
+	public ResponseEntity<?> list(@PathVariable String userIdTo) {
+		try {
+			List<Notification> list = notificationService.list(userIdTo);
+			if(list != null) {
+				return new ResponseEntity<List<Notification>>(list, HttpStatus.OK);
+			} else {
+				return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+			}
+		} catch (Exception e) {
+			return exceptionHandling(e);
+		}
 	}
 	
 	private ResponseEntity<?> exceptionHandling(Exception e) {
