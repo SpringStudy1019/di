0e9c8d071815668ac7cf75274dc45afa27e29e42
a50070863@gmail.com <a50070863@gmail.com>
2023-11-17 10:24:30 +0900

    feat: FileInfo 수정 (#70)
    S3에 저장하기 때문에 객체 URL만 담으면 될 것 같음

--- trend_gaza/src/main/java/com/ssafy/trend_gaza/review/entity/FileInfo.java
+++ trend_gaza/src/main/java/com/ssafy/trend_gaza/review/entity/FileInfo.java
@@ -5,35 +5,18 @@ import lombok.Builder;
 @Builder
 public class FileInfo {
 
-	private String saveFolder;
-	private String originalFile;
 	private String saveFile;
-	
 	
 	public FileInfo() {}
 	
-	public FileInfo(String saveFolder, String originalFile, String saveFile) {
+	
+	public FileInfo(String saveFile) {
 		super();
-		this.saveFolder = saveFolder;
-		this.originalFile = originalFile;
 		this.saveFile = saveFile;
 	}
 	
-	
-	public String getSaveFolder() {
-		return saveFolder;
-	}
-	public String getOriginalFile() {
-		return originalFile;
-	}
 	public String getSaveFile() {
 		return saveFile;
 	}
 
-
-
-	
-	
-	
-	
 }
