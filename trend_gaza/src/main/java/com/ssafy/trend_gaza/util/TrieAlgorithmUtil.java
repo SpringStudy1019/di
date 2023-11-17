998aaa0153693444c5ad92eb945a646e882e1e23
a50070863@gmail.com <a50070863@gmail.com>
2023-11-17 16:33:25 +0900

    feat: Trie 알고리즘 Util 수정 (#93)
    관광지 아이디도 Node에 추가
    관광지 상세 보기 시에 관광지 아이디가 필요함

--- trend_gaza/src/main/java/com/ssafy/trend_gaza/util/TrieAlgorithmUtil.java
+++ trend_gaza/src/main/java/com/ssafy/trend_gaza/util/TrieAlgorithmUtil.java
@@ -7,15 +7,18 @@ import java.util.Map;
 
 import org.springframework.stereotype.Component;
 
+import com.ssafy.trend_gaza.attraction.dto.AttractionAutoSearchResponse;
+
 @Component
 public class TrieAlgorithmUtil {
 
 	public static class Node {
 		Map<Character, Node> chiledNode = new HashMap<Character, Node>();		// 자식노드
 		boolean endOfword;		// 단어의 끝인지 아닌지 체크
+		int contentId;			// 관광지 아이디
 	}
 	
-	List<String> autoCompleteStr;
+	List<AttractionAutoSearchResponse> autoCompleteStr;
 
 	/*
 	 *  Trie자료구조를 생성할 떄 rootNode는 기본적으로 생성
@@ -25,7 +28,7 @@ public class TrieAlgorithmUtil {
 	/*
 	 * Trie에 문자열 저장 
 	 */
-	public void insert(String str) {
+	public void insert(int contentId, String str) {
 		Node node = this.rootNode;		// Trie자료구조는 항상 rootNode부터 시작
 
 		// 문자열의 각 단어마다 가져와서 자식노드 중에 있는지 체크
@@ -35,6 +38,7 @@ public class TrieAlgorithmUtil {
 		}
 		
 		node.endOfword = true;		// 저장 할 문자열의 마지막 단어에 매핑되는 노드에 단어의 끝임을 명시
+		node.contentId = contentId;
 	}
 	
 
@@ -63,8 +67,8 @@ public class TrieAlgorithmUtil {
 	 * str = 사용자가 검색한 문자열
 	 * node = 사용자가 검색한 문자열로 시작하는 문자열을 담은 객체
 	 */
-	public List<String> autoComplete(String str, Node node) {
-		autoCompleteStr = new ArrayList<String>();
+	public List<AttractionAutoSearchResponse> autoComplete(String str, Node node) {
+		autoCompleteStr = new ArrayList<AttractionAutoSearchResponse>();
 		
 		dfs (node, str);
 		
@@ -80,11 +84,18 @@ public class TrieAlgorithmUtil {
 	 */
 	public void dfs(Node node, String str) {
 		if (node.endOfword == true) {		
-			autoCompleteStr.add(str);
+			autoCompleteStr.add(AttractionAutoSearchResponse.builder()
+					.title(str)
+					.contentId(node.contentId)
+					.build());
 		}
 		
 		if (node.chiledNode == null) {
-			autoCompleteStr.add(str);
+			//autoCompleteStr.add(str);
+			autoCompleteStr.add(AttractionAutoSearchResponse.builder()
+					.title(str)
+					.contentId(node.contentId)
+					.build());
 			return;
 		}
 		
