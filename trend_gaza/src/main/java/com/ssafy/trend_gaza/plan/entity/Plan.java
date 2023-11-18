96bb82b2270acec32c9fd033501616d305c43fe3
a50070863 <a50070863@gmail.com>
2023-11-18 21:21:51 +0900

    feat: Plan 엔티티에 제목 필드 추가 (#138)

--- trend_gaza/src/main/java/com/ssafy/trend_gaza/plan/entity/Plan.java
+++ trend_gaza/src/main/java/com/ssafy/trend_gaza/plan/entity/Plan.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 public class Plan {
 	
 	private Integer planIdx;
+	private String title;
 	private LocalDateTime startDate;
 	private LocalDateTime endDate;
 	private String userId;
@@ -12,9 +13,11 @@ public class Plan {
 	
 	public Plan() {}
 
-	public Plan(Integer planIdx, LocalDateTime startDate, LocalDateTime endDate, String userId) {
+	public Plan(Integer planIdx, String title, LocalDateTime startDate,
+			LocalDateTime endDate, String userId) {
 		super();
 		this.planIdx = planIdx;
+		this.title = title;
 		this.startDate = startDate;
 		this.endDate = endDate;
 		this.userId = userId;
@@ -23,6 +26,10 @@ public class Plan {
 	
 	public Integer getPlanIdx() {
 		return planIdx;
+	}
+	
+	public String getTitle() {
+		return title;
 	}
 
 	public LocalDateTime getStartDate() {
