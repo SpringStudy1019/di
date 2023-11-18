001f9745c15085809f9d8d075f23aa0ddee8321e
a50070863 <a50070863@gmail.com>
2023-11-18 20:33:07 +0900

    feat: 레포지토리단 구현 (#134)

--- trend_gaza/src/main/java/com/ssafy/trend_gaza/plan/repository/PlanMapper.java
+++ trend_gaza/src/main/java/com/ssafy/trend_gaza/plan/repository/PlanMapper.java
@@ -4,7 +4,9 @@ import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
+
 @Mapper
 public interface PlanMapper {
 	int registerPlan(Map<String, Object> param);
+	int modifyPlan(Map<String, Object> param);
 }
