bca3af2b9a428956ba44b5659120c0fcc38e3eb6
a50070863 <a50070863@gmail.com>
2023-11-18 20:38:29 +0900

    feat: API 추가 (#134)

--- trend_gaza/src/main/java/com/ssafy/trend_gaza/plan/controller/PlanController.java
+++ trend_gaza/src/main/java/com/ssafy/trend_gaza/plan/controller/PlanController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -34,6 +35,13 @@ public class PlanController {
 			@RequestBody List<PlanRequest> planRequest) {
 		planService.registerPlan(planRequest, planIdx);
 		return ResponseEntity.created(URI.create("")).build();		// URI 경로 지정 필요함
+	}
+	
+	@PutMapping("/{attractionPlanId}")
+	public ResponseEntity<?> modifyPlan(@PathVariable int attractionPlanId,
+			@RequestBody List<PlanRequest> planRequest) {
+		planService.modifyPlan(planRequest, attractionPlanId);
+		return ResponseEntity.ok().build();
 	}
 
 }
