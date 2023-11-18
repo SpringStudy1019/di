6485f6fa56bd2113c35d977ed7a5730e924e32ce
Juhyun Lee <juhyun62015@gmail.com>
2023-11-18 16:25:45 +0900

    feat: FE main view hot places (#132)

--- trend_gaza_FE/src/components/attraction/AttractionRecommend.vue
+++ trend_gaza_FE/src/components/attraction/AttractionRecommend.vue
@@ -29,17 +29,33 @@ const recommend = () => {
             <div class="col-10">
                 <h2>사용자들이 가장 많은 리뷰를 남긴 여행지에요</h2>
                 <!-- 여행지 TOP5-->
-                <div v-for="hotplace in hotplaces" :key="hotplace.idx">
-                       {{ hotplace }} 
-                </div>
+                <div class="card" style="width: 18rem;" v-for="hotplace in hotplaces" :key="hotplace.contentId">
+                    <img class="card-img-top" :src="hotplace.firstImage" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ hotplace.title }} </h5>
+                        <!-- <button class="btn btn-primary me-2">자세히 보기</button> -->
+                        <router-link 
+                        :to="{ name: 'attraction-view', params: { attractionIdx: hotplace.contentId } }" 
+                        class="btn btn-primary me-2">
+                            자세히 보기
+                        </router-link>
+                        <button class="btn btn-primary">찜하기</button>
+                    </div>
+                    </div>
             </div>
             <div class="col-1"></div>
         </div>
     </div>
+
+    <!-- test -->
+   
 </template>
 
 <style scoped>
 #top {
     margin-top: 50px;
 }
+
+
+
 </style>
