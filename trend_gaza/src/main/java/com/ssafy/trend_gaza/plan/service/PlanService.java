14c65558a7a6635303a4604115c7f1ba93cdc48d
a50070863 <a50070863@gmail.com>
2023-11-18 20:34:15 +0900

    feat: 서비스단 구현 (#134)

--- trend_gaza/src/main/java/com/ssafy/trend_gaza/plan/service/PlanService.java
+++ trend_gaza/src/main/java/com/ssafy/trend_gaza/plan/service/PlanService.java
@@ -6,4 +6,5 @@ import com.ssafy.trend_gaza.plan.dto.PlanRequest;
 
 public interface PlanService {
 	int registerPlan(List<PlanRequest> planRequest, int planIdx);
+	int modifyPlan(List<PlanRequest> planRequest, int attractionPlanId);
 }
