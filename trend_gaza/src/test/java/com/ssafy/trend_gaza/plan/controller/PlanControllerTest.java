19ce41aaa985d028d66f72acd0c37be196293687
a50070863 <a50070863@gmail.com>
2023-11-18 20:07:57 +0900

    test: 컨트롤러, 서비스 테스트 추가 (#130)

---
+++ trend_gaza/src/test/java/com/ssafy/trend_gaza/plan/controller/PlanControllerTest.java
@@ -0,0 +1,70 @@
+package com.ssafy.trend_gaza.plan.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ssafy.trend_gaza.plan.dto.PlanRequest;
+import com.ssafy.trend_gaza.plan.service.PlanService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@AutoConfigureMockMvc
+@SpringBootTest(
+		properties = {
+				"spring.config.location=classpath:application.properties"
+		},
+		classes = { PlanController.class }
+)
+@Slf4j
+@ComponentScan(basePackages = {"com.ssafy"})
+public class PlanControllerTest {
+
+	@Autowired
+	private PlanService planService;
+	
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	@Transactional
+	@DisplayName("여행 계획 세우기")
+	void testRegisterPlan() throws Exception {
+		// given
+		List<PlanRequest> planRequest = new ArrayList<PlanRequest>();
+		planRequest.add(PlanRequest.builder()
+				.attractionId(2856807)
+				.order(1)
+				.orderDate(1)
+				.build());
+		planRequest.add(PlanRequest.builder()
+				.attractionId(126479)
+				.order(1)
+				.orderDate(2)
+				.build());
+		ObjectMapper objectMapper = new ObjectMapper();
+		String content = objectMapper.writeValueAsString(planRequest); 
+		
+		// when, then
+		mockMvc.perform(post("/plans/1")
+				.content(content).contentType(MediaType.APPLICATION_JSON))
+		.andExpect(status().isCreated())
+		.andDo(print());
+	}
+}
---
+++ trend_gaza/src/test/java/com/ssafy/trend_gaza/plan/service/PlanServiceImplTest.java
@@ -0,0 +1,55 @@
+package com.ssafy.trend_gaza.plan.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ssafy.trend_gaza.plan.dto.PlanRequest;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest(properties = { "spring.config.location=classpath:application.properties" }, 
+classes = {
+		PlanServiceImpl.class })
+@AutoConfigureMockMvc
+@ComponentScan(basePackages = { "com.ssafy" })
+public class PlanServiceImplTest {
+	
+	@Autowired
+	private PlanService planService;
+	
+	@Test
+	@Transactional
+	@DisplayName("여행 계획 세우기")
+	void testRegisterPlan() {
+		// given
+		List<PlanRequest> planRequests = new ArrayList<PlanRequest>();
+		planRequests.add(PlanRequest.builder()
+				.attractionId(125266)
+				.orderDate(1)
+				.order(1)
+				.build());
+		planRequests.add(PlanRequest.builder()
+				.attractionId(125267)
+				.orderDate(1)
+				.order(2)
+				.build());
+		
+		// when
+		int result = planService.registerPlan(planRequests, 1);
+		
+		// then
+		assertEquals(1, result);
+	}
+
+}
