669e12f271fbad8a27dae3c7cbee6ce2adc0f4b8
a50070863 <a50070863@gmail.com>
2023-11-19 20:31:17 +0900

    style: 현재 코스 리스트 사이드바 추가 (#153)

--- trend_gaza_FE/src/components/plan/PlanMap.vue
+++ trend_gaza_FE/src/components/plan/PlanMap.vue
@@ -164,6 +164,7 @@ const updateData = () => {
   
   <div class="wrapper">
     <button class="btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">🔍</button>
+    <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">Toggle right offcanvas</button>
 
 <div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
   <div class="offcanvas-header">
@@ -173,6 +174,16 @@ const updateData = () => {
   <div class="offcanvas-body">
     <PlanSearch @getAttractionData="loadAttractionList" />
     <PlanSearchList v-for="attraction in attractionList" :attraction="attraction" :key="attraction.contentId"/>
+  </div>  
+</div>
+
+<div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="offcanvasRightLabel">🎒현재 코스 리스트</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    ...
   </div>
 </div>
 
