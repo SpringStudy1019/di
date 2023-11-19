a870df6d8927c95206d8449e85237729a4f94cf5
a50070863 <a50070863@gmail.com>
2023-11-19 15:46:27 +0900

    style: 커스텀 오버레이에 스타일 적용 (#77)

--- trend_gaza_FE/src/components/plan/PlanMap.vue
+++ trend_gaza_FE/src/components/plan/PlanMap.vue
@@ -61,14 +61,37 @@ const loadMarkers = (data) => {
       const marker = new kakao.maps.Marker({
         map: map, // 마커를 표시할 지도
         position: new kakao.maps.LatLng(latitude, longitude),
-        title: data[i].title,
+        title: "마커",
         clickable: true,
       });
 
       markers.value.push(marker);
 
-      // Create content for the custom overlay
-      const content = `<div class="wrap">
+      // Use a function to create a closure for each iteration
+      const createClickHandler = (marker, title) => {
+        return function () {
+          // 커스텀 오버레이에 표시할 컨텐츠 입니다
+          // 커스텀 오버레이는 아래와 같이 사용자가 자유롭게 컨텐츠를 구성하고 이벤트를 제어할 수 있기 때문에
+          // 별도의 이벤트 메소드를 제공하지 않습니다 
+          const content = `
+          <style>
+            .wrap {position: absolute;left: 0;bottom: 40px;width: 288px;height: 132px;margin-left: -144px;text-align: left;overflow: hidden;font-size: 12px;font-family: 'Malgun Gothic', dotum, '돋움', sans-serif;line-height: 1.5;}
+            .wrap * {padding: 0;margin: 0;}
+            .wrap .info {width: 286px;height: 120px;border-radius: 5px;border-bottom: 2px solid #ccc;border-right: 1px solid #ccc;overflow: hidden;background: #fff;}
+            .wrap .info:nth-child(1) {border: 0;box-shadow: 0px 1px 2px #888;}
+            .info .title {padding: 5px 0 0 10px;height: 30px;background: #eee;border-bottom: 1px solid #ddd;font-size: 18px;font-weight: bold;}
+            .info .close {position: absolute;top: 10px;right: 10px;color: #888;width: 17px;height: 17px;background: url('https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/overlay_close.png');}
+            .info .close:hover {cursor: pointer;}
+            .info .body {position: relative;overflow: hidden;}
+            .info .desc {position: relative;margin: 13px 0 0 90px;height: 75px;}
+            .desc .ellipsis {overflow: hidden;text-overflow: ellipsis;white-space: nowrap;}
+            .desc .jibun {font-size: 11px;color: #888;margin-top: -2px;}
+            .info .img {position: absolute;top: 6px;left: 5px;width: 73px;height: 71px;border: 1px solid #ddd;color: #888;overflow: hidden;}
+            .info:after {content: '';position: absolute;margin-left: -12px;left: 50%;bottom: 0;width: 22px;height: 12px;background: url('https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/vertex_white.png')}
+            .info .link {color: #5085BB;}
+          </style>
+          
+          <div class="wrap">
                         <div class="info">
                           <div class="title">
                             ${data[i].title}
@@ -86,37 +109,34 @@ const loadMarkers = (data) => {
                         </div>
                       </div>`;
 
-      // Create custom overlay
-      const overlay = new kakao.maps.CustomOverlay({
-        content: content,
-        map: null, // Do not set the map initially
-        position: marker.getPosition(),
-      });
+          // 마커 위에 커스텀오버레이를 표시합니다
+          // 마커를 중심으로 커스텀 오버레이를 표시하기위해 CSS를 이용해 위치를 설정했습니다
+          var overlay = new kakao.maps.CustomOverlay({
+              content: content,
+              map: map,
+              position: marker.getPosition()       
+          });
 
-      // Attach click event to marker to toggle the custom overlay
-      kakao.maps.event.addListener(marker, 'click', function () {
-        if (overlay.getMap()) {
-          overlay.setMap(null);
-        } else {
-          overlay.setMap(map);
-        }
-      });
+          // 마커를 클릭했을 때 커스텀 오버레이를 표시합니다
+          kakao.maps.event.addListener(marker, 'click', function() {
+            overlay.setMap(map);
+          });
+        };
+      };
+
+      // Attach click event to marker using the function
+      kakao.maps.event.addListener(marker, "click", createClickHandler(marker, data[i].title));
     }
 
-    // Set the map center to the last marker's position
+    // 지도 중심을 이동 시킵니다
     map.setCenter(new kakao.maps.LatLng(latitude, longitude));
   }
 };
 
-// Close the overlay when the close button is clicked
+// 커스텀 오버레이를 닫기 위해 호출되는 함수입니다 
 function closeOverlay() {
-  if (overlay) {
-    overlay.setMap(null);
-  }
+    overlay.setMap(null);     
 }
-
-
-
 
 // 마커 삭제
 function hideMarkers() {
