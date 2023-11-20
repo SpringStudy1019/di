57cdde1c2f114e0a199d29ac9b394db1ce1e2c99
a50070863 <a50070863@gmail.com>
2023-11-21 01:35:52 +0900

    feat: User 수정 및 DTO 추가 (#164)

---
+++ trend_gaza/src/main/java/com/ssafy/trend_gaza/user/dto/ModifyProfileImageRequest.java
@@ -0,0 +1,21 @@
+package com.ssafy.trend_gaza.user.dto;
+
+import lombok.Builder;
+
+@Builder
+public class ModifyProfileImageRequest {
+	
+	private String imgUrl;
+	
+	public ModifyProfileImageRequest() {}
+	
+	
+	public ModifyProfileImageRequest(String imgUrl) {
+		super();
+		this.imgUrl = imgUrl;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+}
