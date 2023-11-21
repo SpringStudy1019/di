152c4eb79b2d0a2380be95103ce44a938555e13a
a50070863@gmail.com <a50070863@gmail.com>
2023-11-21 12:18:29 +0900

    feat: API 수정 (#171)
    AuthenticationUtil에서 현재 로그인 된 사용자 아이디를 가져옴

--- trend_gaza/src/main/java/com/ssafy/trend_gaza/plan/controller/PlanController.java
+++ trend_gaza/src/main/java/com/ssafy/trend_gaza/plan/controller/PlanController.java
@@ -81,6 +81,7 @@ public class PlanController {
 	 */
 	@PostMapping
 	public ResponseEntity<?> setPlan(@RequestBody SetPlanRequest setPlanRequest) {
+		setPlanRequest.setUserId(AuthenticationUtil.getCurrentUserSocialId());
 		planService.setPlan(setPlanRequest);
 		return new ResponseEntity<>(HttpStatus.CREATED);
 	}
