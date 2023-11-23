d7a045e6b5c35b92d0235a45efc2720c7b2fa5a1
likelasttime <46569105+likelasttime@users.noreply.github.com>
2023-11-23 11:50:57 +0900

    [Feature] 로그인 여부에 따라 메뉴를 다르게 보이기 (#202)
    * style: 로그인 시 메뉴바 유지 (#196)
    
    로그인 후 로그아웃을 하면 로그인 버튼으로 전환도 됨
    문제는 그 뒤에 새로고침을 하면 다시 로그인 상태에 뜨는 메뉴가 보임
    
    * fix: 로그인, 로그아웃 에러 해결 (#196)
    
    mount 될때 로그인 여부를 확인함

--- trend_gaza_FE/src/components/plan/PlanSearch.vue
+++ trend_gaza_FE/src/components/plan/PlanSearch.vue
@@ -54,7 +54,7 @@ const searchAttractions = () => {
   searchAttractionsByCondition(
     param.value,
     ({ data }) => {
-      emit("param", param);     // 검색 조건
+      emit("param", param); // 검색 조건
       emit("getAttractionData", data);
       emit("currentPage", data.currentPage);
       emit("totalPage", data.totalPageCount);
@@ -67,80 +67,131 @@ const searchAttractions = () => {
 };
 
 const searchLike = () => {
-  getLikeAttractions(props.planIdx,
-  ({data}) => {
-    emit("bookmarks", data);
-  }, (error) => {
-    console.log(error);
-  }
-  )
+  getLikeAttractions(
+    props.planIdx,
+    ({ data }) => {
+      emit("bookmarks", data);
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
 };
 </script>
 
 <template>
   <div class="search-container">
-    <form class="d-flex my-3" onsubmit="return false;" role="search">
-      <div class="form-group me-2 d-block">
-        <select id="search-area" class="form-select me-2" v-model="sido">
-          <option value="0" selected>검색 할 지역 선택</option>
-          <option value="1">서울</option>
-          <option value="2">인천</option>
-          <option value="3">대전</option>
-          <option value="4">대구</option>
-          <option value="5">광주</option>
-          <option value="6">부산</option>
-          <option value="7">울산</option>
-          <option value="8">세종특별자치시</option>
-          <option value="31">경기도</option>
-          <option value="32">강원특별자치도</option>
-          <option value="33">충청북도</option>
-          <option value="34">충청남도</option>
-          <option value="35">경상북도</option>
-          <option value="36">경상남도</option>
-          <option value="37">전라북도</option>
-          <option value="38">전라남도</option>
-          <option value="39">제주도</option>
-        </select>
+    <form onsubmit="return false;" role="search">
+      <div>
+        <div>
+          <select id="search-area" class="sido-select" v-model="sido">
+            <option value="0" selected>검색 할 지역 선택</option>
+            <option value="1">서울</option>
+            <option value="2">인천</option>
+            <option value="3">대전</option>
+            <option value="4">대구</option>
+            <option value="5">광주</option>
+            <option value="6">부산</option>
+            <option value="7">울산</option>
+            <option value="8">세종특별자치시</option>
+            <option value="31">경기도</option>
+            <option value="32">강원특별자치도</option>
+            <option value="33">충청북도</option>
+            <option value="34">충청남도</option>
+            <option value="35">경상북도</option>
+            <option value="36">경상남도</option>
+            <option value="37">전라북도</option>
+            <option value="38">전라남도</option>
+            <option value="39">제주도</option>
+          </select>
+        </div>
       </div>
 
-      <div class="form-group me-2">
-        <select id="search-content-id" class="form-select me-2" v-model="contentTypeId">
-          <option value="0" selected>관광지 유형</option>
-          <option value="12">관광지</option>
-          <option value="14">문화시설</option>
-          <option value="15">축제공연행사</option>
-          <option value="25">여행코스</option>
-          <option value="28">레포츠</option>
-          <option value="32">숙박</option>
-          <option value="38">쇼핑</option>
-          <option value="39">음식점</option>
-        </select>
+      <div class="col-md-4">
+        <div>
+          <select id="search-content-id" class="type-select" v-model="contentTypeId">
+            <option value="0" selected>관광지 유형</option>
+            <option value="12">관광지</option>
+            <option value="14">문화시설</option>
+            <option value="15">축제공연행사</option>
+            <option value="25">여행코스</option>
+            <option value="28">레포츠</option>
+            <option value="32">숙박</option>
+            <option value="38">쇼핑</option>
+            <option value="39">음식점</option>
+          </select>
+        </div>
       </div>
-      <div class="form-group me-2">
-        <input
-          id="search-keyword"
-          class="form-control me-2"
-          v-model="keyword"
-          type="search"
-          placeholder="검색어"
-          aria-label="검색어"
-        />
+      <div>
+        <div class="keyword">
+          <input
+            class="keyword-input"
+            id="search-keyword"
+            v-model="keyword"
+            type="search"
+            placeholder="검색어"
+            aria-label="검색어"
+          />
+        </div>
       </div>
-      <button
-        id="btn-search"
-        class="btn btn-outline-success"
-        @click="searchAttractions"
-        type="button"
-      >
-        검색
-      </button>
-      <button id="btn-like" @click="searchLike">찜한 관광지 보기</button>
+      <div class="condition-btn">
+        <button id="btn-search" class="btn-search" @click="searchAttractions" type="button">
+          검색
+        </button>
+        <button id="btn-like" class="btn-like" @click="searchLike">찜한 관광지 보기</button>
+      </div>
     </form>
   </div>
 </template>
 
 <style scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+
 .search-container {
   border: 1px black;
+}
+
+.btn-like {
+  background-color: plum;
+  color: white;
+}
+
+.btn-search {
+  width: 100px;
+  margin: 10px;
+  background-color: plum;
+  color: white;
+}
+
+.sido-select {
+  width: 80%;
+  margin: 10px;
+  display: flex;
+  justify-content: center;
+}
+
+.type-select {
+  width: 80%;
+  margin: 10px;
+  display: flex;
+  justify-content: center;
+}
+
+.keyword-input {
+  width: 85%;
+  border: gainsboro solid 2px;
+  border-radius: 5%;
+}
+
+.keyword {
+  margin: 10px;
+}
+
+.condition-btn {
+  margin-bottom: 5px;
+  margin-left: 10px;
 }
 </style>
