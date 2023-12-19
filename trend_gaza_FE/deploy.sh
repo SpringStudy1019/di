144b2bc64b374ff1b910eebea8abc1d9d5af1e76
likelasttime <46569105+likelasttime@users.noreply.github.com>
2023-12-19 22:59:53 +0900

    스크립트 권한 문제 해결 (#246)
    * hotfix: 스크립트 권한 에러 해결 (#245)

--- trend_gaza_FE/deploy.sh
+++ trend_gaza_FE/deploy.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-REPOSITORY=/home/ubuntu/trendgaza_fe
+REPOSITORY=/home/ubuntu/trendgaza_fe/dist
 cd $REPOSITORY
 
-DIST_PATH=$REPOSITORY/dist
-
-echo "> $DIST_PATH 에 쓰기 권한 추가"
-chmod +w $DIST_PATH
-
-echo "> $DIST_PATH 배포"
-cp -r * /var/www/html/.
+echo "> $REPOSITORY 배포"
+sudo cp -r * /var/www/html/.
 
 echo "> nginx 재시작"
 sudo systemctl reload nginx
